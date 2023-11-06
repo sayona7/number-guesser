8346fae805f56506debe0fb23a98dc494626e92a
Veronica Bakanowicz <veronica.bakanowicz@gmail.com>
2023-11-06 15:42:05 -0600

    Created generateTarget()

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -4,3 +4,19 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+// Step 1
+function generateTarget() {
+    const randomNumber = Math.floor(Math.random() * 10);
+    return randomNumber;
+  }
+  //console.log(generateTarget());
+
+  // Step 2
+
+
+  // Step 3
+
+  
+
+  // Step 4
+
