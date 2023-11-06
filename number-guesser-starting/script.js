01c813256719edd78ae76921d109554c73e76441
Veronica Bakanowicz <veronica.bakanowicz@gmail.com>
2023-11-06 16:14:21 -0600

    Completed compareGuesses()

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -13,7 +13,19 @@ function generateTarget() {
 
   // Step 2
   function compareGuesses(human, computer, secret) {
-    
+    if (secret !== human && secret !== computer) {
+        let humanDif = Math.abs(secret - human);
+        let computerDif = Math.abs(secret - computer);
+        if (humanDif === computerDif || humanDif < computerDif) {
+          return true;
+        } else if (humanDif > computerDif) {
+          return false;
+        }
+      } else if (secret === human) {
+        return true;
+      } else if (secret === computer) {
+        return false;
+      }
   }
 
   // Step 3
